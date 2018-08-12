0bae1cc41a936c138333389ea83f2ed90f9b87fd
Ambler <allinlbj@gmail.com>
2018-08-12 18:23:23 +0800

    add lock

--- src/js/upload-song.js
+++ src/js/upload-song.js
@@ -5,7 +5,11 @@
             return $(this.el).find(selector)[0]
         }
     }
-    let model = {}
+    let model = {
+        data: {
+            status: 'open'
+        }
+    }
     let controller = {
         init(view, model) {
             this.view = view
@@ -33,20 +37,26 @@
                 // chunk_size: '4mb', //分块上传时，每片的体积
                 auto_start: true, //选择文件后自动上传，若关闭需要自己绑定事件触发上传
                 init: {
-                    'FilesAdded': function (up, files) {
+                    'FilesAdded': (up, files) => {
                         plupload.each(files, function (file) {
                             // 文件添加进队列后,处理相关的事情
                         });
                     },
-                    'BeforeUpload': function (up, file) {
+                    'BeforeUpload': (up, file) => {
                         // 每个文件上传前,处理相关的事情
                         window.eventHub.emit('beforeUpload')
+                        if(this.model.data.status === 'closed') {
+                            return false
+                        } else {
+                            this.model.data.status = 'closed'
+                            return true
+                        }
                     },
                     'UploadProgress': function (up, file) {
                         // 每个文件上传时,处理相关的事情
                         uploadStatus.textContent = '上传中'
                     },
-                    'FileUploaded': function (up, file, info) {
+                    'FileUploaded': (up, file, info) => {
                         // 每个文件上传成功后,处理相关的事情
                         // 其中 info.response 是文件上传成功后，服务端返回的json，形式如
                         // {
@@ -55,6 +65,7 @@
                         //  }
                         // 参考http://developer.qiniu.com/docs/v6/api/overview/up/response/simple-response.html
                         window.eventHub.emit('afterUpload')
+                        this.model.data.status = 'open'
                         var domain = up.getOption('domain');
                         console.log(domain)
                         var response = JSON.parse(info.response);
