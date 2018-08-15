fceb8b6b839de93a0c488d774bd2fa95703c9460
Ambler <allinlbj@gmail.com>
2018-08-15 17:47:29 +0800

    获取歌曲列表

---
+++ src/js/index/page-1-2.js
@@ -0,0 +1,66 @@
+{
+    let view = {
+        el: 'section.songs',
+        template:`
+        <li>
+        <h3>{{song.name}}</h3>
+        <p>
+          <svg class="icon icon-sq">
+            <use xlink:href="#icon-sq"></use>
+          </svg>
+          {{song.singer}}
+        </p>
+        <a class="playButton" href="#">
+          <svg class="icon icon-play">
+            <use xlink:href="#icon-play"></use>
+          </svg> 
+        </a>
+      </li>
+        `,
+        init() {
+          this.$el = $(this.el)
+        },
+        render(data) {
+          let {songs} = data
+          songs.map((song)=> {
+            // 你知道的我是可以这样创建一个DOM元素的
+            let $li = $(this.template
+              .replace('{{song.name}}',song.name)
+              .replace('{{song.singer}}',song.singer)
+            )
+            this.$el.find('ol.list').append($li)
+          })
+        }
+    }
+
+    let model = {
+      data: {
+        songs:[]
+      },
+      find() {
+        var query = new AV.Query('Song');        
+        return query.find().then((songs) => {
+            this.data.songs = songs.map((song) => {
+                // 这个id可是很重要的，当然在这里的find是批量获取,而不是get！id是用于获取单个对象实例了
+                // 「...」你要的全拿走
+                return {id: song.id, ...song.attributes}
+            })
+            // 拿到什么就返回什么，Promise的特点
+            return songs
+        })
+      }
+    }
+
+    let controller = {
+      init(view, model) {
+        this.view = view
+        this.view.init()
+        this.model = model
+        this.model.find().then(()=>{
+          this.view.render(this.model.data)
+        })
+      }
+    }
+
+    controller.init(view,model)
+}
